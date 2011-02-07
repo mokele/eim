26eca391575c792af7a0e171137fe5110128dc69
Steven Gravell <steve@mokele.co.uk>
2011-02-07 22:48:18 +0000

    testing with riak

--- src/egm.erl
+++ src/egm.erl
@@ -18,8 +18,16 @@ resize(_Bin, _W, _H) ->
 %% ===================================================================
 -ifdef(TEST).
 
-resize_test() ->
-    {ok, Binary} = file:read_file("../priv/board.png"),
-    ResizedBinary = resize(Binary, 200, 150),
-    ok = file:write_file("../priv/board_thumb.jpg", ResizedBinary).
+resize_test_() ->
+    {timeout, 60, fun() ->
+        {ok, Binary} = file:read_file("../priv/board.png"),
+        {ok, Riak} = riakc_pb_socket:start_link("127.0.0.1", 8087),
+        Bucket = <<"images">>,
+        ok = riakc_pb_socket:put(Riak, riakc_obj:new(Bucket, <<"1.jpg">>, resize(Binary, 200, 150), "image/jpg")),
+        ok = riakc_pb_socket:put(Riak, riakc_obj:new(Bucket, <<"2.jpg">>, resize(Binary, 300, 250), "image/jpg")),
+        ok = riakc_pb_socket:put(Riak, riakc_obj:new(Bucket, <<"3.jpg">>, resize(Binary, 400, 350), "image/jpg")),
+        ok = riakc_pb_socket:put(Riak, riakc_obj:new(Bucket, <<"4.jpg">>, resize(Binary, 500, 450), "image/jpg")),
+        ok = riakc_pb_socket:put(Riak, riakc_obj:new(Bucket, <<"5.jpg">>, resize(Binary, 600, 550), "image/jpg")),
+        ok
+    end}.
 -endif.
